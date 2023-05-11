4cc60eecba7c3bd0e7ba218b3ccf5b2487337295
WindyFeg <phongspeed7@yahoo.com.vn>
2023-05-11 11:48:27 +0700

    Delete unes file

--- README.md
+++
@@ -1,2 +0,0 @@
-1. Link Figma 
-https://www.figma.com/file/uaW4UAdOAkX5sk4Kb79hkC/Web-Lab?node-id=1-77&t=p5S7umUJPMYWBWYS-0
--- register.php
+++
@@ -1,7 +0,0 @@
-<!-- products.php -->
-
-<h1>Register</h1>
-
-<a href="http://localhost/index.php?page=home">
-    <button>Go to Home</button>
-</a>
